e95f090fa02c75a33f9555d183d8de0fc46ce82d
Bruno Braga Chagas <bbchagas@gmail.com>
2019-02-25 12:01:45 -0300

    Create a component to handle floating label and slider value  (#39)
    * Create a component to handle floating label on inputs
    
    * Change slider js to properly handle slider value
    
    * Fix code style
    
    Co-Authored-By: brunobc182 <bbchagas@gmail.com>

--- docs/js/inputs.js
+++ docs/js/inputs.js
@@ -1,26 +1,35 @@
-export default window.requestAnimationFrame(() => {
-  const inputs = document.querySelectorAll(".a-input");
+export const initFloatingLabel = () => {
+  window.requestAnimationFrame(() => {
+    const inputs = document.querySelectorAll(".a-input");
 
-  const verifyValue = (input, label) => {
-    if (input.value) {
-      label.classList.add("a-input--floating-label");
-    } else {
-      label.classList.remove("a-input--floating-label");
-    }
-  };
+    const verifyValue = (input, label) => {
+      if (input.value) {
+        label.classList.add("a-input--floating-label");
+      } else {
+        label.classList.remove("a-input--floating-label");
+      }
+    };
 
-  inputs.forEach(input => {
-    const currentInput = input.querySelector("input");
-    const currentLabel = input.querySelector("label");
+    inputs.forEach(input => {
+      const currentInput = input.querySelector("input");
+      const currentLabel = input.querySelector("label");
 
-    verifyValue(currentInput, currentLabel);
-
-    currentInput.addEventListener("focus", () => {
       verifyValue(currentInput, currentLabel);
-    });
 
-    currentInput.addEventListener("focusout", () => {
-      verifyValue(currentInput, currentLabel);
+      currentInput.addEventListener("focus", () => {
+        verifyValue(currentInput, currentLabel);
+      });
+
+      currentInput.addEventListener("focusout", () => {
+        verifyValue(currentInput, currentLabel);
+      });
     });
   });
-});
+};
+
+const HandleFloatingLabel = () => {
+  initFloatingLabel();
+  return null;
+};
+
+export default HandleFloatingLabel;
--- docs/js/slider.js
+++ docs/js/slider.js
@@ -1,12 +1,20 @@
-export default 
+export const changeSliderValue = () => {
   window.requestAnimationFrame(() => {
     const slider = document.querySelector("#slider1");
     const counter = document.querySelector(".a-slider--value");
 
-    slider.addEventListener("input", function () {
+    slider.addEventListener("input", function() {
       const sliderValue = slider.value;
 
       slider.style.setProperty("--slider-percentage", `${sliderValue}%`);
       counter.innerText = `${sliderValue} coins`;
     });
-  })
+  });
+};
+
+const HandleSliderValue = () => {
+  changeSliderValue();
+  return null;
+};
+
+export default HandleSliderValue;
