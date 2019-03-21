43faf527558ce57b542dac93631f4a037bf7b030
El Marques <elson@marques.ws>
2019-03-21 14:33:36 -0300

    Create iconography page on docz (#59)
    * Create iconography page on docz
    
    * Make IconsCollection code cleaner
    
    * Beautify some CSS code :nail_care:
    
    * Master branch rebase
    
    * Apply text suggestions on Iconography page
    
    * Set default icon color
    
    * Change playground sample text on Iconography page

---
+++ docs/components/IconsCollection.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../css/icons-collection.css';
+
+export const Card = ({ icon }) => (
+  <div className='icons-collection__card'>
+    <div className='icons-collection__card-icon'>
+      <i className={`a-icon a-icon__${icon} a-icon--size-big`} />
+    </div>
+    <p className='icons-collection__card-description'>{icon}</p>
+  </div>
+);
+
+const IconsCollection = ({ icons }) => (
+  <div className='icons-collection'>
+    {icons && icons.map(icon => <Card key={icon} icon={icon} />)}
+  </div>
+);
+
+export default IconsCollection;
