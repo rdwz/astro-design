8e956f69ff66a016b3db0f4105021a1aa35d44d4
Lari Maza <larissa.maza@gmail.com>
2019-06-24 14:47:40 -0300

    Update Iconography with new icons (#120)
    Introduce following icons: dollars, floater, circle more and circle less.

--- docs/components/IconsCollection.jsx
+++ docs/components/IconsCollection.jsx
@@ -4,7 +4,7 @@ import '../css/icons-collection.css';
 export const Card = ({ icon }) => (
   <div className='icons-collection__card'>
     <div className='icons-collection__card-icon'>
-      <i className={`a-icon a-icon__${icon} a-icon--size-big`} />
+      <i className={`a-icon a-icon__${icon}`} />
     </div>
     <p className='icons-collection__card-description'>{icon}</p>
   </div>
