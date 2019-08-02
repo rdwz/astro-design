rdwz/astro-design

commit 633f900e9822db22ebf9cb49b918704221c00f5e

Restore the theme on Astro docs (#137)

docs/theme/index.js
@@ -1,0 +1,288 @@
+const playgroundSelector = '& .Playground__StyledPreviewWrapper-hucttz';
+const selectors = {
+  wrapper: '& .Page__Wrapper-fZfVLY',
+  main: '& .Page__Container-eUbTcM',
+  logo: '& .Logo__LogoText-cJLgZg',
+  menuArea: '& .Sidebar__Wrapper-dMteFe',
+  innerMenuArea: '& .Sidebar__Content-ijXItf',
+  menuItem:
+    '& .MenuLink__Wrapper-bIIltX > a, & .MenuLink__Wrapper-bIIltX > a.active, & .MenuLink__Wrapper-bIIltX > a:visited',
+  menuSubitemsArea: '& .MenuHeadings__Submenu-gcrNS',
+  menuSubItem: '& .SmallLink__Link-jgOnXv, & .SmallLink__Link-jgOnXv:visited',
+  menuBtn: '& .Hamburger__ToggleButton-hPUXGI',
+  menuBtnShape: '& .Hamburger__IconLine-bUJqMY',
+  mobileLogoArea: '& .Logo__Wrapper-bzwKYh.kYsbLl',
+  mobileMenu: '& .Sidebar__Wrapper-dMteFe',
+  playgroundWrapper: playgroundSelector,
+  playgroundChildrens: `${playgroundSelector} > div`,
+
+  /* Docz default elements (hidden) */
+
+  editPageBtn: '& .Page__EditPage-jqcgPa',
+  githubBtn: 'a[aria-label="View source on Github"]',
+  builtDocz: '& .Sidebar__Footer-eORsBO',
+  search: '& .Search__Wrapper-jMtieA',
+  menuBlueTop: '& .Logo__Wrapper-bzwKYh:before',
+  menuItemsBorder: '& .MenuLink__Wrapper-bIIltX:after',
+  menuSubitemsBorder: '& .MenuLink__Wrapper-bIIltX.gUhHxl:after',
+  headingIcon: '& .heading--Icon'
+};
+
+export default {
+  styles: {
+    body: `
+      ${selectors.editPageBtn} {
+        display: none;
+      }
+
+      ${selectors.githubBtn} {
+        display: none;
+      }
+
+      ${selectors.builtDocz} {
+        display: none;
+      }
+
+      ${selectors.search} {
+        display: none;
+      }
+
+      ${selectors.menuBlueTop} {
+        display: none;
+      }
+
+      ${selectors.menuItemsBorder} {
+        display: none;
+      }
+
+      ${selectors.menuSubitemsBorder} {
+        display: none;
+      }
+
+      ${selectors.headingIcon} {
+        display: none;
+      }
+
+      ${selectors.wrapper} {
+        background-image: var(--gradient-nebulosa);
+        background-position: top;
+        background-repeat: no-repeat;
+        background-size: 100% 312px;
+      }
+
+      @media (min-width: 920px) {
+        ${selectors.main} {
+          width: 90%;
+        }
+      }
+
+      @media (min-width: 768px) {
+        ${selectors.main} {
+          padding: 0 40px 40px 90px;
+        }
+      }
+
+      ${selectors.logo} {
+        height: 171px;
+        width: 151px;
+        margin: 46px 60px;
+        background-image: url("/public/astro-logo.svg");
+        background-size: 164px;
+        background-position: center;
+        background-repeat: no-repeat;
+        text-indent: -999px;
+      }
+
+      & p {
+        font: var(--font-secondary);
+        color: var(--color-moon-900) !important;
+      }
+
+      a {
+        color: var(--color-uranus-700);
+      }
+
+      ${selectors.menuArea} {
+        background-color: var(--color-space-100);
+        border-right: 1px solid var(--color-space-300);
+        width: 320px;
+
+        ${selectors.innerMenuArea} {
+          width: 320px;
+        }
+      }
+
+      ${selectors.menuItem} {
+        font-family: 'Poppins', sans-serif;
+        font-size: 20px;
+        font-weight: 500;
+        color: var(--color-moon-900);
+        border-top: 1px solid var(--color-space-300);
+        padding: 22px 50px;
+
+        &:hover {
+          color: var(--color-moon-300);
+          font-weight: 500;
+        }
+
+        &::after {
+          position: absolute;
+          right: 32px;
+          width: 32px;
+          height: 32px;
+          content: '';
+          mask-image: url('public/drop-down.svg');
+          background-color: var(--color-moon-900);
+        }
+
+        &.active::after {
+          mask-image: url('public/drop-up.svg');
+          background-color: var(--color-moon-900);
+        }
+      }
+
+      @media (max-width: 1119px) {
+        ${selectors.menuItem} {
+          padding: 22px 39px;
+        }
+      }
+
+      ${selectors.menuSubitemsArea} {
+        margin: 5px 0 15px;
+      }
+
+      ${selectors.menuSubItem} {
+        font-family: 'Poppins', sans-serif;
+        font-size: 16px;
+        font-weight: 500;
+        color: var(--color-moon-400);
+        padding: 6px 50px;
+        opacity: 1;
+
+        &:hover {
+          color: var(--color-moon-300);
+        }
+
+        &.active {
+          color: var(--color-moon-900);
+        }
+
+        &::before {
+          display: none;
+        }
+      }
+
+      ${selectors.menuBtn} {
+        background-color: transparent;
+
+        &::before {
+          display: none;
+        }
+      }
+
+      ${selectors.menuBtnShape} {
+        height: 0;
+        border: 1px solid var(--color-space-100);
+        border-radius: 3px;
+        background-color: var(--color-space-100);
+      }
+
+      ${selectors.mobileLogoArea} {
+        padding: 0;
+
+        &::before {
+          display: none;
+        }
+      }
+
+      ${selectors.mobileMenu} {
+        background-color: var(--color-space-100);
+      }
+
+      @media (max-width: 560px) {
+        & h1.-bdhje3 {
+          font-size: 48px;
+        }
+      }
+
+      ${selectors.playgroundWrapper} {
+        padding: 16px;
+        overflow-x: scroll;
+      }
+
+      ${selectors.playgroundChildrens} {
+        & > * {
+          box-sizing: border-box;
+          margin: 12px;
+        }
+
+        & > .a-slider {
+          margin-bottom: 22px;
+        }
+
+        @media (min-width: 780px) {
+          &.md\\:two-columns {
+            display: grid;
+            grid-template-columns: auto auto;
+          }
+        }
+
+        &.gradient-bg {
+          background-image: var(--gradient-andromeda);
+        }
+      }
+    `,
+    h1: `
+      font-family: 'Poppins', sans-serif;
+      font-size: 64px;
+      font-weight: 600;
+      letter-spacing: normal;
+      color: var(--color-space-100);
+      margin: 103px 0 186px;
+
+      ::before {
+        display: 'none';
+      }
+    `,
+    h2: `
+      font-family: 'Poppins', sans-serif;
+      font-size: 24px;
+      font-weight: 600;
+      color: var(--color-moon-500);
+      border-bottom: none;
+      margin: 0 0 15px;
+
+      & a[href] {
+        color: var(--color-uranus-700);
+        text-decoration: underline;
+
+        :hover {
+          color: var(--color-uranus-500);
+        }
+
+        :active {
+          color: var(--color-uranus-800);
+        }
+      }
+
+      &#manifesto, &#get-started {
+        font-size: 40px;
+        color: var(--color-moon-900);
+        margin-top: 64px;
+      }
+    `,
+    h3: `
+      font-family: 'Poppins', sans-serif;
+      font-size: 16px;
+      font-weight: 600;
+      color: var(--color-moon-500);
+    `,
+    h4: `
+      font-family: 'Lato', sans-serif;
+      font-size: 22px;
+      font-weight: 400;
+      color: var(--color-moon-400);
+      margin-bottom: 65px;
+    `
+  }
+};

docs/buttons.mdx
@@ -87,7 +87,7 @@ To create an iconlabel button, first create a regular primary button with all si
 
 If you want the icon to the right of the label, add the `a-btn--iconlabel-right` in addition to the previous settings and place the `<i>` tag inside the button and after its inner text, instead.
 
-See our [Iconography](https://astro.magnetis.com.br/#/docs-iconography) to learn how to use Astro icons.
+See our [Iconography](https://astro.magnetis.com.br/iconography) to learn how to use Astro icons.
 
 <Playground>
   <button className='a-btn a-btn--uranus a-btn--medium a-btn--iconlabel'>
@@ -157,7 +157,7 @@ Icon buttons can be used for simple microinteractions and commands.
 
 To create an icon button, first create a regular primary button with all size and color modifier classes, plus the `a-btn--icon` class. Then add a `<i>` tag inside the button with no inner text.
 
-See our [Iconography](https://astro.magnetis.com.br/#/docs-iconography) to learn how to use Astro icons.
+See our [Iconography](https://astro.magnetis.com.br/iconography) to learn how to use Astro icons.
 
 <Playground>
   <button className='a-btn a-btn--uranus a-btn--medium a-btn--icon'>
@@ -183,7 +183,7 @@ Outline icon buttons can be used for simple microinteractions and commands.
 
 To create an outline icon button, first create a regular outline button with all size and color modifier classes, plus the `a-btn--icon` class. Then add a `<i>` tag inside the button with no inner text.
 
-See our [Iconography](https://astro.magnetis.com.br/#/docs-iconography) to learn how to use Astro icons.
+See our [Iconography](https://astro.magnetis.com.br/iconography) to learn how to use Astro icons.
 
 <Playground>
   <button className='a-btn a-btn--outline-uranus a-btn--medium a-btn--icon'>
@@ -212,7 +212,7 @@ Ghost icon buttons can be used for simple microinteractions and commands.
 
 To create a ghost icon button, first create a regular ghost button with all size and color modifier classes, plus the `a-btn--icon` class. Then add a `<i>` tag inside the button with no inner text.
 
-See our [Iconography](https://astro.magnetis.com.br/#/docs-iconography) to learn how to use Astro icons.
+See our [Iconography](https://astro.magnetis.com.br/iconography) to learn how to use Astro icons.
 
 <Playground>
   <button className='a-btn a-btn--ghost-uranus a-btn--medium a-btn--icon'>
@@ -356,7 +356,7 @@ When creating small or big iconlabel or icon buttons, remember to change the siz
 
 For inline links in paragraphs and informative texts. Regular links navigate to other pages or external content.
 
-To create a link, combine the `a-link` class with an Astro typography class in your markup to apply our link properties over your desired font settings. See example below and the [Typography](/docs-typography) page for detailed usage of typography classes.
+To create a link, combine the `a-link` class with an Astro typography class in your markup to apply our link properties over your desired font settings. See example below and the [Typography](https://astro.magnetis.com.br/typography) page for detailed usage of typography classes.
 
 If you're looking for buttons in `<a>` tags, see "buttons in `<a>` elements" below.
 

docs/components/GetStartedCards.jsx
@@ -17,14 +17,19 @@ export default () => (
 
         <li>
           <p>
-            Get our font links in the <a href='https://astro.magnetis.com.br/#/docs-typography'>
+            Get our font links in the{' '}
+            <a href='https://astro.magnetis.com.br/typography'>
               Typography page
-            </a> and load them in your project.
+            </a>{' '}
+            and load them in your project.
           </p>
         </li>
       </ul>
 
-      <a className='a-btn a-btn--earth a-btn--medium' href='https://github.com/magnetis/astro/blob/master/README.md#adding-astro-to-your-project'>
+      <a
+        className='a-btn a-btn--earth a-btn--medium'
+        href='https://github.com/magnetis/astro/blob/master/README.md#adding-astro-to-your-project'
+      >
         read install docs
       </a>
     </div>
@@ -36,13 +41,14 @@ export default () => (
           <p>Astro components are atomic and can be used in isolation.</p>
         </li>
         <li className='code-snippet'>
-          <pre>
-            {codesnippet}
-          </pre>
+          <pre>{codesnippet}</pre>
         </li>
       </ul>
 
-      <a className='a-btn a-btn--earth a-btn--medium' href='https://github.com/magnetis/astro/blob/master/README.md#using-astro'>
+      <a
+        className='a-btn a-btn--earth a-btn--medium'
+        href='https://github.com/magnetis/astro/blob/master/README.md#using-astro'
+      >
         view usage docs
       </a>
     </div>
@@ -65,12 +71,16 @@ export default () => (
 
         <li>
           <p>
-            Run <code>yarn docz:dev</code> and visit <a href='http://localhost:3000/'>localhost:3000</a>
+            Run <code>yarn docz:dev</code> and visit{' '}
+            <a href='http://localhost:3000/'>localhost:3000</a>
           </p>
         </li>
       </ul>
 
-      <a className='a-btn a-btn--earth a-btn--medium' href='https://github.com/magnetis/astro/blob/master/CONTRIBUTING.md'>
+      <a
+        className='a-btn a-btn--earth a-btn--medium'
+        href='https://github.com/magnetis/astro/blob/master/CONTRIBUTING.md'
+      >
         see contributing guide
       </a>
     </div>

docs/iconography.mdx
@@ -93,9 +93,9 @@ All icons have a 32x32px bound as default format. They can be resized in our 16p
 
 Pick one icon name above and apply the following class structure:
 
-<div className='code-snippet'>
-  <pre>{'<i className="a-icon a-icon__icon-name" />'}</pre>
-</div>
+```html
+<i className="a-icon a-icon__icon-name" />
+```
 
 ### sizes
 

docs/index.mdx
@@ -4,6 +4,7 @@ route: '/'
 order: 1
 ---
 
+import '../src/index.css';
 import './css/home.css';
 import GithubButtons from './components/GithubButtons';
 import ManifestoCards from './components/ManifestoCards';

docs/inputs.mdx
@@ -162,7 +162,7 @@ This input style does not have a disabled state.
 
 These inputs are commonly used for money input data, in contexts that require increasing and decreasing the value in large amounts while still enabling users to type in numbers manually.
 
-They carry outline [icon buttons](/docs-buttons#icon-buttons) and these will always add or subtract R$ 100 or R$ 1.000.
+They carry outline [icon buttons](https://astro.magnetis.com.br/buttons#icon-buttons) and these will always add or subtract R$ 100 or R$ 1.000.
 
 The buttons will need JS to work on your project. You may find the code for this working example in our GitHub repository. We also used a plugin for the currency mask, as explained in the Masked Inputs section above.
 
@@ -216,8 +216,8 @@ To create the disabled state, add the `a-input--disabled` modifier class on elem
 
 ## messaging inputs
 
-Inputs used for messaging and similar contexts. They carry embedded [icon buttons](/docs-buttons#icon-buttons) that send the data,
-and their color can be changed as explained in the [Buttons](/docs-buttons) page.
+Inputs used for messaging and similar contexts. They carry embedded [icon buttons](https://astro.magnetis.com.br/buttons#icon-buttons) that send the data,
+and their color can be changed as explained in the [Buttons](https://astro.magnetis.com.br/buttons) page.
 
 Click inside to view `:focus` state.
 

doczrc.js
@@ -1,33 +1,10 @@
 import { css } from 'docz-plugin-css';
-
-const selectors = {
-  wrapper: '& .css-pw2o4y',
-  main: '& .css-1i0fr1k',
-  logo: '& .css-c9p757',
-  menuArea: '& .css-wwgiek',
-  innerMenuArea: '& .css-pccrlp',
-  menuItem: '& .css-82rrzo, .css-82rrzo.active, .css-82rrzo:visited',
-  menuSubitemsArea: '& .css-1qc5yjq',
-  menuSubItem: '& .css-1l1jc95, .css-1l1jc95:visited',
-  menuBtn: '& .css-6pue65, .css-15exo54',
-  menuBtnShape: '& .css-14xo6fr, .css-1v3qlja',
-  mobileLogoArea: '& .css-12xm67f',
-  mobileMenu: '& .css-k33w14',
-
-  /* Docz default elements (hidden) */
-
-  editPageBtn: '& .css-p4m8gg',
-  githubBtn: 'a[aria-label="View source on Github"]',
-  builtDocz: '& .css-b4nn6f',
-  search: '& .css-1pxi6pq',
-  menuBlueTop: '& .css-y1r3ra:before',
-  menuItemsBorder: '& .css-1ccg7te:after',
-  menuSubitemsBorder: '& .css-1evit71:after'
-};
+import astroTheme from './docs/theme';
 
 export default {
-  hashRouter: true,
   base: '/',
+  src: './docs',
+  public: './public',
   plugins: [
     css({
       preprocessor: 'postcss' /* stylelint-disable-line */
@@ -41,226 +18,14 @@ export default {
   htmlContext: {
     favicon: 'public/favicon.png'
   },
-  themeConfig: {
-    /* In this file, the font shorthand with variable only works if you'll use it whole don't need to alter any other font properties */
-    styles: {
-      body: {
-        /* Hide Docz default elements */
-
-        [selectors.editPageBtn]: {
-          display: 'none'
-        },
-        [selectors.githubBtn]: {
-          display: 'none'
-        },
-        [selectors.builtDocz]: {
-          display: 'none'
-        },
-        [selectors.search]: {
-          display: 'none'
-        },
-        [selectors.menuBlueTop]: {
-          display: 'none'
-        },
-        [selectors.menuItemsBorder]: {
-          display: 'none'
-        },
-        [selectors.menuSubitemsBorder]: {
-          display: 'none'
-        },
-
-        /* Custom theme styles */
-
-        [selectors.wrapper]: {
-          /* Gradient header */
-          backgroundImage: 'var(--gradient-nebulosa)',
-          backgroundPosition: 'top',
-          backgroundRepeat: 'no-repeat',
-          backgroundSize: '100% 312px'
-        },
-        '@media (min-width: 920px)': {
-          [selectors.main]: {
-            width: '90%'
-          }
-        },
-        '@media (min-width: 768px)': {
-          [selectors.main]: {
-            padding: '0 40px 40px 90px'
-          }
-        },
-        [selectors.logo]: {
-          height: 171,
-          width: 151,
-          margin: '46px 60px',
-          backgroundImage: 'url("public/astro-logo.svg")',
-          backgroundSize: 164,
-          backgroundPosition: 'center',
-          backgroundRepeat: 'no-repeat',
-          textIndent: -999
-        },
-        '& p': {
-          font: 'var(--font-secondary)',
-          color: 'var(--color-moon-900)!important'
-        },
-        a: {
-          color: 'var(--color-uranus-700)'
-        },
-        [selectors.menuArea]: {
-          backgroundColor: 'var(--color-space-100)',
-          borderRight: '1px solid var(--color-space-300)',
-          width: 320,
-          [selectors.innerMenuArea]: {
-            width: 320
-          }
-        },
-        [selectors.menuItem]: {
-          fontFamily: "'Poppins', sans-serif",
-          fontSize: 20,
-          fontWeight: 500,
-          color: 'var(--color-moon-900)',
-          borderTop: '1px solid var(--color-space-300)',
-          padding: '22px 50px',
-          '&:hover': {
-            color: 'var(--color-moon-300)',
-            fontWeight: 500
-          },
-          '&:after': {
-            position: 'absolute',
-            right: 32,
-            width: 32,
-            height: 32,
-            content: '""',
-            maskImage: 'url("public/drop-down.svg")',
-            backgroundColor: 'var(--color-moon-900)'
-          },
-          '&.active:after': {
-            maskImage: 'url("public/drop-up.svg")',
-            backgroundColor: 'var(--color-moon-900)'
-          }
-        },
-        '@media (max-width: 1119px)': {
-          [selectors.menuItem]: {
-            padding: '22px 39px'
-          }
-        },
-        [selectors.menuSubitemsArea]: {
-          margin: '5px 0 15px'
-        },
-        [selectors.menuSubItem]: {
-          fontFamily: "'Poppins', sans-serif",
-          fontSize: 16,
-          fontWeight: 500,
-          color: 'var(--color-moon-400)',
-          padding: '6px 50px',
-          opacity: 1,
-          '&:hover': {
-            color: 'var(--color-moon-300)'
-          },
-          '&.active': {
-            color: 'var(--color-moon-900)'
-          },
-          '&:before': {
-            display: 'none'
-          }
-        },
-        [selectors.menuBtn]: {
-          backgroundColor: 'transparent',
-          '&:before': {
-            display: 'none'
-          }
-        },
-        [selectors.menuBtnShape]: {
-          height: 0,
-          border: '1px solid var(--color-space-100)',
-          borderRadius: 3,
-          backgroundColor: 'var(--color-space-100)'
-        },
-        [selectors.mobileLogoArea]: {
-          padding: 0,
-          '&:before': {
-            display: 'none'
-          }
-        },
-        [selectors.mobileMenu]: {
-          backgroundColor: 'var(--color-space-100)'
-        },
-        '@media (max-width: 560px)': {
-          '& h1.css-bdhje3': {
-            /* Specificity hack for responsive h1 */
-            fontSize: 48
-          }
-        }
-      },
-      h1: {
-        fontFamily: "'Poppins', sans-serif",
-        fontSize: 64,
-        fontWeight: 600,
-        letterSpacing: 'normal',
-        color: 'var(--color-space-100)',
-        margin: '103px 0 186px',
-        ':before': {
-          /* Removes bottom blue line */
-          display: 'none'
-        }
-      },
-      h2: {
-        fontFamily: "'Poppins', sans-serif",
-        fontSize: 24,
-        fontWeight: 600,
-        color: 'var(--color-moon-500)',
-        borderBottom: 'none',
-        margin: '0 0 15px',
-        '& a[href]': {
-          /* Selects download links in Typography page */
-          color: 'var(--color-uranus-700)',
-          textDecoration: 'underline',
-          '&:hover': {
-            color: 'var(--color-uranus-500)'
-          },
-          '&:active': {
-            color: 'var(--color-uranus-800)'
-          }
-        },
-        '&#manifesto, &#get-started': {
-          /* Selects specific Homepage h2 headings */
-          fontSize: 40,
-          color: 'var(--color-moon-900)',
-          marginTop: 64
-        }
-      },
-      h3: {
-        fontFamily: "'Poppins', sans-serif",
-        fontSize: 16,
-        fontWeight: 600,
-        color: 'var(--color-moon-500)'
-      },
-      h4: {
-        fontFamily: "'Lato', sans-serif",
-        fontSize: 22,
-        fontWeight: 400,
-        color: 'var(--color-moon-400)',
-        marginBottom: 65
-      },
-      playground: {
-        padding: 16,
-        overflowX: 'scroll',
-        '& > *': {
-          boxSizing: 'border-box',
-          margin: 12
-        },
-        '& > .a-slider': {
-          marginBottom: 22
-        },
-        '@media (min-width: 780px)': {
-          '&.md\\:two-columns': {
-            display: 'grid',
-            gridTemplateColumns: 'auto auto'
-          }
-        },
-        '&.gradient-bg': {
-          backgroundImage: 'var(--gradient-andromeda)'
-        }
-      }
-    }
-  }
+  themeConfig: astroTheme,
+  menu: [
+    'home',
+    'colors',
+    'buttons',
+    'controls & toggles',
+    'iconography',
+    'inputs',
+    'typography'
+  ]
 };

commit e54d49aa99bd234b48dc2c05589eeccd20751163

FIx minor layout breaks introduced by PR #137 (#141)

docs/theme/index.js
@@ -14,7 +14,8 @@ const selectors = {
   mobileLogoArea: '& .Logo__Wrapper-bzwKYh.kYsbLl',
   mobileMenu: '& .Sidebar__Wrapper-dMteFe',
   playgroundWrapper: playgroundSelector,
-  playgroundChildrens: `${playgroundSelector} > div`,
+  playgroundChildren: `${playgroundSelector} > div`,
+  headerTitle: '& .H1-ecIwba',
 
   /* Docz default elements (hidden) */
 
@@ -31,6 +32,9 @@ const selectors = {
 export default {
   styles: {
     body: `
+      overflow: auto;
+      line-height: 1.5;
+
       ${selectors.editPageBtn} {
         display: none;
       }
@@ -105,10 +109,15 @@ export default {
       ${selectors.menuArea} {
         background-color: var(--color-space-100);
         border-right: 1px solid var(--color-space-300);
-        width: 320px;
+
+        @media (min-width: 1120px) {
+          width: 320px;
+        }
 
         ${selectors.innerMenuArea} {
-          width: 320px;
+          @media (min-width: 1120px) {
+            width: 320px;
+          }
         }
       }
 
@@ -200,7 +209,7 @@ export default {
       }
 
       @media (max-width: 560px) {
-        & h1.-bdhje3 {
+        ${selectors.headerTitle} {
           font-size: 48px;
         }
       }
@@ -210,7 +219,7 @@ export default {
         overflow-x: scroll;
       }
 
-      ${selectors.playgroundChildrens} {
+      ${selectors.playgroundChildren} {
         & > * {
           box-sizing: border-box;
           margin: 12px;
@@ -228,6 +237,8 @@ export default {
         }
 
         &.gradient-bg {
+          padding: 16px;
+          margin: -16px;
           background-image: var(--gradient-andromeda);
         }
       }

src/css/checkbox.css
@@ -3,6 +3,7 @@
 
   display: flex;
   position: relative;
+  align-items: center;
 }
 
 .a-checkbox > label {
@@ -39,7 +40,7 @@
 
 .a-checkbox__shape::after {
   position: absolute;
-  top: 7px;
+  top: 5px;
   left: 3px;
   width: 10px;
   height: 6px;
@@ -51,7 +52,7 @@
 }
 
 .a-checkbox--indeterminate > .a-checkbox__shape::after {
-  top: 11px;
+  top: 9px;
   left: 4px;
   width: 8px;
   transform: rotate(-180deg);