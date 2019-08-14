558a25bae15f847b040b00af9bc7e65ba21d4abf
Alysson Lopes <LopesAlysson@users.noreply.github.com>
2019-08-14 16:57:11 -0300

    Add step on deploy.js to create 404.html file (#153)

--- deploy.js
+++ deploy.js
@@ -3,12 +3,17 @@ const gh = require('gh-pages');
 
 const DIST_PATH = '.docz/dist';
 
-fs.copyFile('CNAME', `${DIST_PATH}/CNAME`, (err) => {
+fs.copyFile('CNAME', `${DIST_PATH}/CNAME`, err => {
   if (err) throw err;
   console.log('CNAME file copied to .docz/dist directory');
 });
 
-gh.publish(DIST_PATH, (err) => {
+fs.copyFile(`${DIST_PATH}/index.html`, `${DIST_PATH}/404.html`, err => {
+  if (err) throw err;
+  console.log('index.html file copied as 404.html');
+});
+
+gh.publish(DIST_PATH, err => {
   if (err) {
     console.log('Ops, something went wrong...', err);
   } else {
